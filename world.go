e6f0c3613de9c6ed3bbf111ccd4adbfb75d72068
pengsz <pengshzh@chinatelecom.cn>
2021-08-31 19:32:36 +0800

    second commit

--- world.go
+++ world.go
@@ -3,9 +3,9 @@ package projectc
 import "fmt"
 
 func SayWorld() {
-	fmt.Println("Say World, version 1")
+	fmt.Println("Say World, version 1.0.0")
 }
 
 func World() string {
-	return "World, version 1"
+	return "World, version 1.0.0"
 }
